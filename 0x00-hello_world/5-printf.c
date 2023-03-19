5d48dbfcd9dcd10366f87c21ace076e55f8cfe29
emino <eminvash@gmail.com>
2023-03-19 14:29:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
- /**
-  * main - 'This is the main function'
-  * return: 0 (success)
-  */
+/**
+ * main - this is a main function
+ * Return: 0
+ */
 int main(void);
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
